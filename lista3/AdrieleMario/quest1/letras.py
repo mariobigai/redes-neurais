66c9ba8886fcccec93cc7cb658c59d6b96bb34c5
Mario-LICON <mariobigai1@gmail.com>
2023-11-16 16:35:34 -0300

    Modificando prints das estatísticas, criando os testes e organizando diretório

---
+++ lista3/AdrieleMario/quest1/letras.py
@@ -0,0 +1,45 @@
+'''
+Nome: letras.dat
+Quantidade de memórias fundamentais: 12
+Dimensão da grade: 20 x 20
+'''
+from hopfield import *
+import matplotlib.pyplot as plt
+
+# Números = [1, 2, 3, 4] representados por matriz(9,5) de pixeis (Branco:1, Preto:-1)
+letras = pd.read_table('letras.txt', sep=' ')
+
+#Cria objeto hopfield
+hp = hopfield(patterns=letras.values, noise_percentage=0.50,
+              pattern_n_row=20, pattern_n_column=20, ib=0, epochs=100000, neta = 0.1)
+hp.run()
+
+## Calculando Acertos e Erro médio
+for elemento, out in zip(letras.values, hp.outputs.values):
+    acerto = 0
+    erro = 0
+    print(100*'-')
+    for i in range(hp.nrow*hp.ncol):
+        if elemento[i] != out[i]:
+            erro += 1
+        else:
+            acerto += 1
+
+    print('Pixels certos: ' + str(acerto))
+    print('Pixels errados: ' + str(erro))
+    med_perc = erro * 100 / (hp.nrow * hp.ncol)
+    print(f'Erro medio %: {med_perc:.4f}')
+
+# #Plotando resultados
+fig, axs = plt.subplots(nrows=3, ncols=12, figsize=(10, 15))
+
+for j in range(len(letras.values)):
+    axs[0][j].set_title(f'Amostra {j+1}')
+    axs[0][j].imshow(letras.values[j].reshape(hp.nrow,hp.ncol), cmap='Grays')
+
+    axs[1][j].set_title(f'Ruido em {j+1}')
+    axs[1][j].imshow(hp.noised_img.iloc[j,:].values.reshape(hp.nrow,hp.ncol), cmap='Grays')
+
+    axs[2][j].set_title(f'Saída {j+1}')
+    axs[2][j].imshow(hp.outputs.iloc[j,:].values.reshape(hp.nrow,hp.ncol), cmap='Grays')
+plt.show()
